5a9d6e33c733f6d39f61f23dacd23e0fe2f86a94
khajiitos <khajiit112@gmail.com>
2023-08-21 05:24:42 +0200

    Remove unused imports

--- Forge/src/main/java/me/khajiitos/iswydt/forge/EventListeners.java
+++ Forge/src/main/java/me/khajiitos/iswydt/forge/EventListeners.java
@@ -1,22 +1,12 @@
 package me.khajiitos.iswydt.forge;
 
 import me.khajiitos.iswydt.common.ISeeWhatYouDidThere;
-import me.khajiitos.iswydt.common.action.PlaceFluidRecord;
 import me.khajiitos.iswydt.common.action.StartFireActionRecord;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.BaseFireBlock;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.player.FillBucketEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventListeners {
