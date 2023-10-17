0d2cecd2171473c6c25f1d323bd60fa4f9aad37b
khajiitos <khajiit112@gmail.com>
2023-10-17 19:45:18 +0200

    Don't load client-sided classes on server-side
    (cherry picked from commit eac6141715bdd86426953ce05ba83853e641e0ea)

--- Forge/src/main/java/me/khajiitos/iswydt/forge/ISeeWhatYouDidThereForge.java
+++ Forge/src/main/java/me/khajiitos/iswydt/forge/ISeeWhatYouDidThereForge.java
@@ -3,8 +3,10 @@ package me.khajiitos.iswydt.forge;
 import me.khajiitos.iswydt.common.ISeeWhatYouDidThere;
 import me.khajiitos.iswydt.common.config.ClothConfigCheck;
 import me.khajiitos.iswydt.common.config.ClothConfigScreenMaker;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,8 +16,10 @@ public class ISeeWhatYouDidThereForge {
         ISeeWhatYouDidThere.init();
         MinecraftForge.EVENT_BUS.register(new EventListeners());
 
-        if (ClothConfigCheck.isInstalled()) {
-            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ClothConfigScreenMaker::create));
-        }
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            if (ClothConfigCheck.isInstalled()) {
+                ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ClothConfigScreenMaker::create));
+            }
+        });
     }
 }
